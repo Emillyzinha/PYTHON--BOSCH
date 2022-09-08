7d1eddc68c5e85d980ab366d7ff7aeae2ce8ffc8
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2022-09-08 13:01:29 -0300

    while, for, range

---
+++ Aula05/ex4.py
@@ -0,0 +1,9 @@
+def binario():
+    bin = int(input("Digite um número para transformação em decimal"))
+    while
+
+    # bin2 = bin%2
+    # while bin2 < 0:
+    #     print(bin2)
+if __name__=="__main__":
+    binario()

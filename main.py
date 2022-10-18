21cbc5a586e652d057b1ae684e0bd1ab99955adb
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2022-10-18 11:22:42 -0300

    jogo_da_velha

--- main.py
+++ main.py
@@ -1,76 +1,98 @@
-import random
-from bonecos import*
+# def jogo_da_velha():
 
-def comeco():
-    print("-"*46)
-    print("        BEM-VINDO AO JOGO DA FORCA!")
-    print("-" * 46)
-
-def jogo_forca():
-    chances = 6
-    palavras_lista = ["BOSCH", "ETS", "JOVEM", "SMART", "PYTHON"]
-    dicas = ["Empresa", "Setor aprendiz", "Aprendiz", "Curso dentro da BOSCH", "Linguagem de programação"]
-    palavra_aleatoria = random.choice(palavras_lista)
-    palavra_certa = []
-    for i in range(len(palavra_aleatoria)):
-        palavra_certa.append(('-'))
-    erros = []
-    print('\nPalavra:', '-' * len(palavra_aleatoria))
-
-
+def menu():
+    lista = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print("------------ JOGO DA VELHA ------------")
     while True:
-        for j in range(len(palavras_lista)):
-            if palavra_aleatoria == palavras_lista[j]:
-                print(f"\nA dica é: {dicas[j]}")
-        letra = input("Digite uma letra: ").upper().replace(" ", "")
-
-
-        while erros.__contains__(letra) or palavra_certa.__contains__(letra):
-            letra = input("Você já digitou essa letra, digite outra diferente: ").replace(" ", "").upper()
-
-        if letra.isdigit():
-            print("Digite uma letra, não um número.")
-            continue
-
-        if len(letra) > 1:
-            print("Digite apenas uma letra.")
-            continue
-
-        if palavra_aleatoria.__contains__(letra):
-            for pos, encima in enumerate(palavra_aleatoria):
-                if letra == encima:
-                    palavra_certa.pop(pos)
-                    palavra_certa.insert(pos, letra)
-                    bonecos(chances)
-                    print("Palavra: ", *palavra_certa)
-            if "".join(palavra_certa) == palavra_aleatoria:
-                print("Você ganhou! :)")
-                escolha()
+        try:
+            jogador_um = int(input("[1] - O\n"
+                                   "[2] - X\n"
+                                   "Você é o jogador 1, escolha a opção acima desejada: "))
+            if jogador_um == 1:
+                print("Você é o jogador 2, sua opção para jogo é o 'X'.")
                 break
-        else:
-            chances -= 1
-            bonecos(chances)
-            if chances < 1:
-                print("Você perdeu :(")
-                print(palavra_aleatoria)
-                escolha()
+            elif jogador_um == 2:
+                print("Você é o jogador 2, sua opção para jogo é o 'O'.")
                 break
-            erros.append(letra)
-            print(f"\nSeus erros são: {erros}")
-            print(f"Você errou e tem {chances} chances\n")
-            print("Palavra: ", *palavra_certa)
+            if jogador_um > 2 or jogador_um < 1:
+                print("Escolha uma das opções apresentadas.\n")
+                continue
+        except(ValueError):
+            print("Digite um número entre as opções apresentadas.\n")
+    printjogo(lista)
+
+    for i in range(0,9):
+        print("--- JOGADOR 1 ---")
+        opcao_um = int(input("Digite a posição desejada: "))
+        substituicao(opcao_um, opcao_dois="", lista=lista)
+        printjogo(lista)
+        vencer(lista)
+
+        print("--- JOGADOR 2 ---")
+        opcao_dois = int(input("Digite a posição desejada: "))
+        substituicao(opcao_um, opcao_dois, lista)
+        printjogo(lista)
 
 
-def escolha():
-    while True:
-        decisao = input("Deseja continuar? Se sim dê enter, caso não digite 'SAIR': \n").upper().replace(" ", "")
-        if decisao == "":
-            jogo_forca()
-        if decisao == "SAIR":
+def substituicao(opcao_um, opcao_dois, lista):
+    for linha in range(0, 3):
+        if opcao_um in lista[linha]:
+            posicao = lista[linha].index(opcao_um)
+            while opcao_um in lista[linha] == "X" or opcao_um in lista[linha] == "O":
+                print("Digite outra opção disponível.")
+            else:
+                lista[linha][posicao] = "O"
+
+    # [linha][posicao]
+    # [linha][posicao]
+
+    for linha in range(0, 3):
+        if opcao_dois in lista[linha]:
+            posicao = lista[linha].index(opcao_dois)
+            if lista[linha][posicao] == "X" or lista[linha][posicao] == "O":
+                print("Digite outra opção disponível.")
+            else:
+                lista[linha][posicao] = "X"
+
+def vencer(lista):
+    for linha1 in range(0,3):
+        if lista[linha1][0] and lista[linha1][1] and lista[linha1][2] == "O":
+            print("Você ganhou jogador 1!")
+        elif lista[linha1][0] and lista[linha1][1] and lista[linha1][2] == "X":
+            print("Você ganhou jogador 2!")
             break
-        else:
-            continue
+
+    if lista[0][0] and lista[1][1] and lista[1][2] == "O":
+        print("Você ganhou jogador 1!")
+    if lista[0][0] and lista[1][1] and lista[1][2] == "X":
+        print("Você ganhou jogador 2!")
+
+    if lista[2][0] and lista[1][1] and lista[0][2] == "O":
+        print("Você ganhou jogador 1!")
+    if lista[2][0] and lista[1][1] and lista[0][2] == "X":
+        print("Você ganhou jogador 2!")
+
+    if lista[0][0] and lista[1][0] and lista[2][0] == "O":
+        print("Você ganhou jogador 1!")
+    if lista[0][1] and lista[1][1] and lista[2][1] == "O":
+        print("Você ganhou jogador 1!")
+    if lista[0][2] and lista[1][2] and lista[2][2] == "O":
+        print("Você ganhou jogador 1!")
+
+    if lista[0][0] and lista[1][0] and lista[2][0] == "X":
+        print("Você ganhou jogador 2!")
+    if lista[0][1] and lista[1][1] and lista[2][1] == "X":
+        print("Você ganhou jogador 2!")
+    if lista[0][2] and lista[1][2] and lista[2][2] == "X":
+        print("Você ganhou jogador 2!")
+
+
+
+def printjogo(lista):
+    print(f" {lista[0][0]} | {lista[0][1]} | {lista[0][2]}\n"
+          f" {lista[1][0]} | {lista[1][1]} | {lista[1][2]}\n"
+          f" {lista[2][0]} | {lista[2][1]} | {lista[2][2]}\n")
 
 if __name__ == '__main__':
-    comeco()
-    jogo_forca()
+    # jogo_da_velha()
+    menu()

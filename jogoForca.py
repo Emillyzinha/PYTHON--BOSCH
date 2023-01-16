5e106199d9bfb9f88e684e3ef6cf9eb5f4371433
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2023-01-16 15:08:03 -0300

    Update jogoForca.py

--- jogoForca.py
+++ jogoForca.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit
 from PyQt5 import QtGui
+
 
 class Forca(QMainWindow):
     def __init__(self):
@@ -14,7 +15,16 @@ class Forca(QMainWindow):
         self.fundo.move(0, 0)
         self.fundo.resize(1000, 600)
         self.fundo.setScaledContents(True)
-        self.fundo.setPixmap(QtGui.QPixmap('img/interface/velha.jpg'))
+        self.fundo.setPixmap(QtGui.QPixmap('img/interface/forca.jpg'))
+
+        self.botao_avada = QPushButton(self)
+        self.botao_avada.move(150, 460)
+        self.botao_avada.resize(190, 80)
+
+        self.caixa_texto = QLineEdit(self)
+        self.caixa_texto.setStyleSheet('border: 2px solid rgb(52, 59, 72, 20%); border-radius: 5px; background-color: rgb(52, 59, 72, 20%);color: rgb(205, 195, 255);')
+        self.caixa_texto.move(120, 325)
+        self.caixa_texto.resize(300, 40)
 
         self.carregar_janela()
 
